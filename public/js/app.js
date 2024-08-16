b08c674cfcef3664be5a4911fedcf4375b1f3aeb
distravantari <distravantari@gmail.com>
2024-08-16 18:19:24 +0700

    add service worker registered

--- public/js/app.js
+++ public/js/app.js
@@ -770,3 +770,13 @@ function preloadPages() {
 }
 
 preloadPages();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch(function(error) {
+      console.log('Service Worker registration failed:', error);
+    });
+  });
+}
